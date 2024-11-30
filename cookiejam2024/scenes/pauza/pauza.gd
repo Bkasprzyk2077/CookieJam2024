8b79fe545eb91dc81854589ba9210336c38cd1fc
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-11-30 23:54:47 +0100

    working pause

--- cookiejam2024/scenes/pauza/pauza.gd
+++ cookiejam2024/scenes/pauza/pauza.gd
@@ -3,9 +3,14 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func wznow():
+	visible = false
 	get_tree().paused = false
 
-
+func pause():
+	visible = true
+	get_tree().paused = true
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		pause()
